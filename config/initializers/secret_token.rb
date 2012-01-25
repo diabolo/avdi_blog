e99b4d886702eb7c45f1d6eb0945e099aa8a07ad
Andrew Premdas <apremdas@yahoo.co.uk>
2012-01-25 14:04:44 +0000

    Rails initialisation
    - ran rails new . -T

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AvdiBlog::Application.config.secret_token = '56dd999f1ceaf2b269f7d6e2dfe8c3938a8a54c2a017beb3addac8f7ee7e9d7c227fe7f107f3b4566660b42381c5c698c36d54c95358d997e60dfee8a3fcba0e'
